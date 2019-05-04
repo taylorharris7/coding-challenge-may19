3344fc11d5718ea9beea50730b9c4262bf98959b
taylorharris7 <44948728+taylorharris7@users.noreply.github.com>
2019-05-04 12:52:05 -0700

    Day 4 Challenge

---
+++ Day4.js
@@ -0,0 +1,32 @@
+function powerOn() {
+  ship.powerOn = true;
+}
+
+function countModules() {
+  return availableModules.length;
+}
+
+function countEssential() {
+  var essentialModule = 0;
+  
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].essential) {
+      essentialModule++;
+    }
+  }
+  
+  return essentialModule;
+}
+
+function loadModule(index) {
+  ship.modules.push(availableModules[index]);
+}
+
+function loadIntoShip() {
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].name == 'life-support') {
+      loadModule(i);
+    }
+  }
+}
+loadIntoShip();
