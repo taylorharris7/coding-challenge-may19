e91e9dc13d0c2a783e5dd015f620b11ad4adccd5
taylorharris7 <44948728+taylorharris7@users.noreply.github.com>
2019-05-07 12:07:43 -0700

    Day 7 Challenge

---
+++ Day7.js
@@ -0,0 +1,41 @@
+function powerOn() {
+  ship.powerOn = true;
+}
+
+function countModules() {
+  return availableModules.length;
+}
+
+function countEssential() {
+  var essentialModule = 0;
+  
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].essential) {
+      essentialModule++;
+    }
+  }
+  
+  return essentialModule;
+}
+
+function findModuleIndex(moduleName) {
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].name == moduleName && availableModules[i].essential) {
+      return i;
+    }
+  }
+}
+
+function loadModule(index) {
+  ship.modules.push(availableModules[index]);
+}
+loadModule(findModuleIndex('life-support'));
+loadModule(findModuleIndex('propulsion'));
+loadModule(findModuleIndex('navigation'));
+
+function resetLARRY() { 
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+resetLARRY();
