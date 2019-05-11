67c741cb41ab800f7eefa1b6ce2f81df07e8a657
taylorharris7 <44948728+taylorharris7@users.noreply.github.com>
2019-05-11 09:34:30 -0700

    Day 11 Challenge

---
+++ Day11.js
@@ -0,0 +1,56 @@
+function powerOn() {
+  ship.powerOn = true;
+}
+
+function countModules() {
+  return availableModules.length;
+}
+
+function countEssential() {
+  var essentialModule = 0;
+  
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].essential) {
+      essentialModule++;
+    }
+  }
+  
+  return essentialModule;
+}
+
+function findModuleIndex(moduleName) {
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].name == moduleName && availableModules[i].essential) {
+      return i;
+    }
+  }
+}
+
+function loadModule(index) {
+  ship.modules.push(availableModules[index]);
+}
+loadModule(findModuleIndex('life-support'));
+loadModule(findModuleIndex('propulsion'));
+loadModule(findModuleIndex('navigation'));
+loadModule(findModuleIndex('communication'));
+
+function resetLARRY() { 
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+resetLARRY();
+
+function setMessage() {
+  radio.message = JSON.stringify(navigation);
+}
+setMessage();
+
+function activateBeacon() {
+  radio.beacon = true;
+}
+activateBeacon();
+
+function setFrequency() {
+  radio.frequency = (radio.range.low + radio.range.high) / 2;
+}
